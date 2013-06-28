c428c047d5809da3907ae9ce049255b520a642f0
TGedanke <TGedanke@097f7491-7637-a856-7271-a34200ca55b7>
2013-06-28 07:04:23 +0000

    При входе в систему отображаются последние внесенные изменения.

--- branches/fp_help/FpAgent/app/view/mainform/Loginformcontainer.js
+++ branches/fp_help/FpAgent/app/view/mainform/Loginformcontainer.js
@@ -3,12 +3,45 @@ Ext.define('FPAgent.view.mainform.Loginformcontainer', {
 	alias : 'widget.loginformcontainer',
 	requires : ['FPAgent.view.mainform.Loginform'],
 	layout : {
-		type : 'vbox',
-		align : 'center',
-		pack : 'center'
+		type : 'hbox',
+		align : 'middle'
 	},
 	items : [{
-			xtype : 'loginform'
+			flex : 1,
+			xtype : 'container'
+		}, {
+			flex : 1,
+			xtype : 'container',
+			minWidth : 270,
+			layout : {
+				type : 'vbox'
+			},
+			items : [{
+					xtype : 'loginform'
+				}
+			]
+		}, {
+			width : 270,
+			height : '100%',
+			xtype : 'container',
+			layout : {
+				type : 'vbox',
+				pack : 'end'
+			},
+			items : [{
+					xtype : 'panel',
+					title : 'ФлипПост WEB - список изменений',
+					width : 240,
+					height : 250,
+					autoScroll : true,
+					bodyPadding : 5,
+					loader : {
+						url : 'help/fpWeb.txt',
+						autoLoad : true,
+						renderer : 'html'
+					}
+				}
+			]
 		}
 	]
 });
