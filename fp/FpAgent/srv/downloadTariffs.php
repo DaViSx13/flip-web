e6d4b1a3c381b7845b3043d3b32582685f92717e
DaViSx13 <DaViSx13@gmail.com>
2016-02-29 09:40:49 +0300

    тарифный план по-умолчанию №2 - "Агент Стандарт"

--- fp/FpAgent/srv/downloadTariffs.php
+++ fp/FpAgent/srv/downloadTariffs.php
@@ -2,11 +2,8 @@
 /*закрузка тарифов с сервера: для корректной работы на сервере должны быть файлы с тарифами TariffsFlipPost<номер тарифа в таблице Klient поле PlanNo>.xls*/
 session_start();
 $agp = $_SESSION['xAgentPlanNo'];
-$file = ("tmpfolder/TariffsFlipPost{$agp}.xls");
-if (!file_exists($file)) {
-$file = ("tmpfolder/TariffsFlipPost.xls");
-};
-$rName='Тарифы ФлипПост.xls';
+$file = ("tmpfolder/tarifs20160301_plan_{$agp}.xls");
+$rName='Тарифы ФлипПост с 01.03.2016.xls';
 header ("Content-Type: application/octet-stream");
 header ("Accept-Ranges: bytes");
 header ("Content-Length: ".filesize($file)); 
