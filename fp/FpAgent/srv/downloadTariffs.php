f7c36eaec3101df7e3ceefa4ba0ae737243d1e93
DaViSx13 <DaViSx13@gmail.com>
2016-03-28 15:16:34 +0300

    новые тарифы с 1.04.2016

--- fp/FpAgent/srv/downloadTariffs.php
+++ fp/FpAgent/srv/downloadTariffs.php
@@ -2,8 +2,8 @@
 /*закрузка тарифов с сервера: для корректной работы на сервере должны быть файлы с тарифами TariffsFlipPost<номер тарифа в таблице Klient поле PlanNo>.xls*/
 session_start();
 $agp = $_SESSION['xAgentPlanNo'];
-$file = ("tmpfolder/tarifs20160301_plan_{$agp}.xls");
-$rName='Тарифы ФлипПост с 01.03.2016.xls';
+$file = ("tmpfolder/tarifs20160401_plan_{$agp}.xls");
+$rName='Тарифы ФлипПост с 01.04.2016.xls';
 header ("Content-Type: application/octet-stream");
 header ("Accept-Ranges: bytes");
 header ("Content-Length: ".filesize($file)); 
