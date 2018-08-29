27ff202bda5cba8846a3e407435332179c426ded
tgedanke <tgedanke@gmail.com>
2018-08-29 20:08:02 +0300

    IS_124: Тариф все работает, нужны 2 файла на сервере с тарифами.

--- fp/fplk/srv/downloadTariffs.php
+++ fp/fplk/srv/downloadTariffs.php
@@ -1,8 +1,8 @@
 <?php
 /*закрузка тарифов с сервера: для корректной работы на сервере должны быть файлы с тарифами TariffsFlipPost<номер тарифа в таблице Klient поле PlanNo>.xls*/
-session_name("AGENTSESSIONID");
+session_name("LKSESSIONID");
 session_start();
-$agp = $_SESSION['xAgentPlanNo'];
+$agp = $_SESSION['xClientPlanNo'];
 $file = ("tmpfolder/tarifs20171201_plan_{$agp}.xlsx");
 $rName='Тарифы ФлипПост с 01.12.2017.xlsx';
 header ("Content-Type: application/octet-stream");
