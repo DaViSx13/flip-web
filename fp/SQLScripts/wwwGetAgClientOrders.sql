a1ff1968728fcde0fdca8fb3c84f97a76e80ec93
tgedanke <tgedanke@gmail.com>
2020-03-19 10:38:25 +0300

    правильный запрос sql

--- fp/SQLScripts/wwwGetAgClientOrders.sql
+++ fp/SQLScripts/wwwGetAgClientOrders.sql
@@ -1,24 +1,23 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [ALERT_F]
+GO
+
+/****** Object:  StoredProcedure [dbo].[wwwGetAgClientOrders]    Script Date: 03/19/2020 10:35:17 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[wwwGetAgClientOrders]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[wwwGetAgClientOrders]
+GO
+
+USE [ALERT_F]
+GO
+
+/****** Object:  StoredProcedure [dbo].[wwwGetAgClientOrders]    Script Date: 03/19/2020 10:35:17 ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE wwwGetAgClientOrders
+
+
+CREATE PROCEDURE [dbo].[wwwGetAgClientOrders]
 @period varchar(10), @agentID int
 AS
 BEGIN
@@ -26,13 +25,9 @@ if @agentID is null return
 if ISNULL(ltrim(rtrim(@period)), '') = '' set @period = left(CONVERT(varchar(50), GETDATE(), 112), 6) + '01'
 
 declare @bDate date, @eDate date, @agID int
-declare @CACC varchar(50), @aUser varchar(50)
 
-select @CACC = CACC, @aUser = aUser from wwwClientUser where agentID = @agentID
-
---set @agID = @agentID -- 54
 set @bDate = @period+'01'
---set @eDate = dateadd(d, -1, DATEADD(m,1,@bDate))
+
 set @eDate = DATEADD(m,1,@bDate)
  
 SELECT [ROrdNum]--
@@ -64,29 +59,32 @@ SELECT [ROrdNum]--
 	case
 	when Wb_no is null then
 		case
-			when RegOrders.NeedDel = 1 then 'отменен'
-			when RegOrders.Wb_no is NULL and (RegOrders.NeedDel is NULL or RegOrders.NeedDel=0) and DATEDIFF(dd, dbo.addWDays(courDate, 2), getDate())>0 then 'просрочено'
-			when RegOrders.Status = 0  then 'заявлен'
-			when RegOrders.Status = 1  then 'передан агенту'
-			when RegOrders.Status = 2  then 'принят агентом'
-			when RegOrders.Status = 4  then 'присвоен № нак.'
-			when RegOrders.Status = 3  then 'отказ клиента'
+			when f.NeedDel = 1 then 'отменен'
+			when f.Wb_no is NULL and (f.NeedDel is NULL or f.NeedDel=0) and DATEDIFF(dd, dbo.addWDays(courDate, 2), getDate())>0 then 'просрочено'
+			when f.Status = 0  then 'заявлен'
+			when f.Status = 1  then 'передан агенту'
+			when f.Status = 2  then 'принят агентом'
+			when f.Status = 4  then 'присвоен № нак.'
+			when f.Status = 3  then 'отказ клиента'
 		end
 	else
 		case --обратный порядок
-			when (select tdd from Main m where m.Wb_No = RegOrders.Wb_no) is not null then 'доставлено'
-			when exists(select 1 from MnfBdy mb where Wb_no = RegOrders.Wb_no and mb.MnfRefNo like 'MOW%') then 'отправлено'
-			when exists(select 1 from MnfBdy mb where Wb_no = RegOrders.Wb_no and mb.MnfRefNo = '0') then 'на комплектовке'
+			when (select tdd from Main m where m.Wb_No = f.Wb_no) is not null then 'доставлено'
+			when exists(select 1 from MnfBdy mb where Wb_no = f.Wb_no and mb.MnfRefNo like 'MOW%') then 'отправлено'
+			when exists(select 1 from MnfBdy mb where Wb_no = f.Wb_no and mb.MnfRefNo = '0') then 'на комплектовке'
 		else 'присвоен № нак.'
 		end
 	end    
      ,[wb_no]
     
-  FROM RegOrders
---  where [DateIn] between @bDate and @eDate
+  FROM AgOrders f
   where [DateIn] >= @bDate and [DateIn] < @eDate
-  and (@agID = -1 or (UserIn=@aUser) and PCACC = @CACC)
+  and (/*@agID = -1 or*/ PCACC = @agentID)
   order by ROrdNum desc
 
 END
+
 GO
+
+grant execute on [dbo].[wwwGetAgClientOrders] to [pod]
+go
