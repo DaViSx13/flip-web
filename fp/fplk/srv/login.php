e997db44c11aac8cd6c1f6e692da9a493b792bb2
tgedanke <tgedanke@gmail.com>
2020-04-15 19:58:52 +0300

    fix

--- fp/fplk/srv/login.php
+++ fp/fplk/srv/login.php
@@ -42,6 +42,7 @@ if ( !empty( $_POST['user'] )) {
                 }; 
         }; 
     }
-	
+header('Access-Control-Allow-Origin: https://flippost.com');
+header('Access-Control-Allow-Credentials: true');	
 echo json_encode($response);
 ?>
