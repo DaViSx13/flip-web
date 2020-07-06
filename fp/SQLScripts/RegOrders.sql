5888c312f7b57a5dc638462f8d39b43d50d53265
tgedanke <tgedanke@gmail.com>
2020-07-06 07:18:50 +0300

    fix

--- fp/SQLScripts/RegOrders.sql
+++ fp/SQLScripts/RegOrders.sql
@@ -1,3 +1,5 @@
 use ALERT_F
-exec sp_rename 'RegOrders.ORGAdressIndex', 'ORGzip', 'COLUMN';
-exec sp_rename 'RegOrders.DESTAdressIndex', 'DESTzip', 'COLUMN';v
+GO
+ALTER TABLE RegOrders
+ADD DESTzip int null,
+	ORGzip int null;
