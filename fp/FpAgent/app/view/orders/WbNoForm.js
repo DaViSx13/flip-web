4787ffe4c61df81c6089a18777091aa3f9f78373
tgedanke <tgedanke@gmail.com>
2020-07-08 13:55:50 +0300

    fix del err comits

--- fp/FpAgent/app/view/orders/WbNoForm.js
+++ fp/FpAgent/app/view/orders/WbNoForm.js
@@ -1,78 +1,21 @@
 Ext.define('FPAgent.view.orders.WbNoForm', {
 	alias : 'widget.wbnoform',
 	extend : 'Ext.form.Panel',
-	requires : ['Ext.grid.plugin.RowEditing',
-				'Ext.grid.RowNumberer'],
-	width  : 400,
-	height : 300,
-	layout : 'fit',
+	width : 758,
+	height : 38,
+	layout : {
+		type : 'hbox'
+	},
 	bodyPadding : 5,
 	items : [{
-		xtype 	: 'gridpanel',
-		viewConfig: {
-			stripeRows: false,
-			getRowClass: function(record) {
-				if (record.get('isdeleted') == 1) {
-					return "colorDeleted-row"
-				}
-			}
-		},
-		flex    : 1,
-		store	: 'WbNoSt',
-		plugins:[{
-			ptype			:'rowediting',
-			clicksToEdit	: 0,
-			pluginId		: "editRow"
-		}],
-		tbar:[{
-				iconCls: 'add',
-				text: 'Добавить',
-				action: 'addRecord',
-				iconAlign: 'left'
+			xtype : 'textfield',
+			name : 'rordnum',
+			hidden : true
 		}, {
-				iconCls: 'eyeOpened',
-				text: 'Показать удаленные',
-				action: 'showDeleted',
-				iconAlign: 'left'
-		}, {
-				text : FPAgent.lib.Translate.tr("OrdTool.action.wbview"),//'Просмотр накладной',
-				iconCls : 'wbview',
-				action : 'wbview'
-		}],
-		columns: [{
-			xtype		: 'rownumberer',
-			header		: '№'
-		}, {
-			header		: 'Номер накладной',
-			dataIndex	: 'web_num',
-			width		: '60%',
-			editor		: {
-				xtype 		: 'textfield',
-				autoBlank	: false
-			}
-		}, {
-			header		: 'Стоимость',
-			dataIndex	: 'cost',
-			flex		: 1,
-			editor		: {
-				xtype 		: 'numberfield',
-				autoBlank	: false
-			}
-		}, {
-			xtype		: 'actioncolumn',
-			width		: '5%',
-			renderer	: function(value, meta, record) {
-				let isDel = record.get('isdeleted');
-
-				return (isDel == 0)
-					   ? '<img src="resources/images/exit.gif" />'
-					   : '<img src="resources/images/restore.png" width="15" height="15"/>';
-			},
-
-			items		: [{
-				xtype	: 'button',
-				text	: 'Удалить',
-			}]
-		}]
-	}]
+			xtype : 'textfield',
+			width : 154,
+			enableKeyEvents : true,
+			name : 'wbno'
+		}
+	]
 });
--- fp/FpAgent/app/view/orders/WbNoWin.js
+++ fp/FpAgent/app/view/orders/WbNoWin.js
@@ -4,12 +4,10 @@ Ext.define('FPAgent.view.orders.WbNoWin', {
 	alias : 'widget.wbnowin',
 	requires : ['FPAgent.view.orders.WbNoForm'],
 	title : FPAgent.lib.Translate.tr("WbNoWin.title"),//'Введите № накладной',
-	title : 'Список накладных',
 	layout : 'fit',
 	autoShow : true,
-	height : 300,
-	width : 400,
-	closable: false,
+	height : 95,
+	width : 180,
 	resizable : false,
 	modal : true,
 	initComponent : function () {
@@ -18,15 +16,12 @@ Ext.define('FPAgent.view.orders.WbNoWin', {
 			}
 		];
 		this.buttons = [{
-			xtype	: 'label',
-			text	: 'Синхронизаия',
-			name	: 'webNoStatusBar',
-			hidden	: true,
-			flex	: 1,
-			height	: '100%'
-		}, {
-				text : 'Закрыть',
-				action : 'syncData'
+				text : FPAgent.lib.Translate.tr("WbNoWin.button.save"),//'Сохранить',
+				action : 'save'
+			}, {
+				text : FPAgent.lib.Translate.tr("WbNoWin.button.cancel"),//'Отмена',
+				scope : this,
+				handler : this.close
 			}
 		];
 		this.callParent(arguments);
