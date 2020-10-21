a1adbf360900d2dec711ecaad11fe45626a0f1b1
tgedanke <tgedanke@gmail.com>
2020-10-21 21:40:36 +0300

    fix

--- fp/fplk/srv/WebWbsGroupReport.php
+++ fp/fplk/srv/WebWbsGroupReport.php
@@ -6,23 +6,23 @@
 
 // Боевой сервер
 // TODO Расскоментировать перед выставлением
-/*
-    $url = 'http://jasperadmin:jasperadmin@10.10.10.6:8080' +
-        '/jasperserver' +
-        '/rest_v2' +
-        '/reports' +
-        '/flippost' +
-        '/reports' +
-        '/wbreportGROUP.pdf?wbNo='.$wbno;
-*/
-    // Локальный сервер
-    $url = 'http://jasperadmin:jasperadmin@192.168.56.5:8081' .
+
+    $url = 'http://jasperadmin:jasperadmin@10.10.10.6:8080'.
         '/jasperserver'.
         '/rest_v2'.
         '/reports'.
         '/flippost'.
         '/reports'.
         '/wbreportGROUP.pdf?wbNo='.$wbno;
+
+    // Локальный сервер
+ /*   $url = 'http://jasperadmin:jasperadmin@192.168.56.5:8081' .
+        '/jasperserver'.
+        '/rest_v2'.
+        '/reports'.
+        '/flippost'.
+        '/reports'.
+        '/wbreportGROUP.pdf?wbNo='.$wbno;*/
     $ch = curl_init($url);
     $fh = fopen('php://temp', 'w');
     curl_setopt($ch, CURLOPT_FILE, $fh);
