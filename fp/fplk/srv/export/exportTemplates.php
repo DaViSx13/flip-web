4accac9e2e3a19f56dbf4f8b3dd317ee3eee9377
vb010894 <234098sam>
2021-02-17 01:22:51 +0300

    Добавлен экспорт шаблонов в кабинете Москвы

---
+++ fp/fplk/srv/export/exportTemplates.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "ExcelExport.php";
+require_once "../secureCheck.php";
+use FlipPost\ExcelExport;
+
+
+$excelHeaders = array(
+    array("TemplateName", "Наименование шаблона", ),
+    array("DName", "Название компании получателя"),
+    array("DContName", "Контактное лицо получателя"),
+    array("DContPhone", "Телефон контактого лица получателя"),
+    array("DAdr", "Адрес получателя"),
+    array("DEST", "Город доставки получателя"));
+
+$ag = isset($params['newAgent']) ? $params['newAgent'] : $_SESSION['xClientID'];
+
+$t = new ExcelExport($excelHeaders, "exec wwwLKgetTemplates @clientID='{$ag}'");
+$t->export();
+
+
+
+
