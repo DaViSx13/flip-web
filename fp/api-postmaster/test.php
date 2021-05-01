fb5b2c362d2603880ae5f3b3e1712fc74dd9f2fc
dvs <DaViSx13@gmail.com>
2021-05-01 11:53:04 +0300

    api-postmaster

---
+++ fp/api-postmaster/test.php
@@ -0,0 +1,17 @@
+﻿<?php				
+
+$status = "Доставлено_ивановйцукен";
+//$status = "Доставлено zxc";
+print_r($status);
+
+
+preg_match("/(Доставлено)[^а-яА-Яa-zA-Z]*([а-яА-Яa-zA-Z]+)/i", $status, $matches); 
+
+print_r($matches);
+
+echo "[[[[";
+print_r( trim(str_replace(["Доставлено", ".",",","_", "  "], "", $status) ) );
+echo "]]]]" ;
+
+
+?>
