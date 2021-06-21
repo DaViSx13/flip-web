21a69870a551c678f8b35d55f714452c286bcb2b
vb010894 <234098sam>
2021-06-21 17:29:17 +0300

    Изменена таблица зказов

---
+++ fp/SQLScripts/ALTER RegOrders.sql
@@ -0,0 +1,8 @@
+Use ALERT_F
+ALTER TABLE RegOrders ADD
+	sberQuick int null,
+	sberSize varchar(100) null,
+	sberCost float null,
+	sberProject varchar(100) null,
+	sberSuit varchar(100) null,
+	sberType int null
