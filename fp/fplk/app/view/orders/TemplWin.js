a958059a0bdaf08e4d069fb2e1e5e6f879561976
vb010894 <aleksiev_vova@mail.ru>
2022-11-01 18:42:00 +0300

    Изменено Кабинет Москвы  1) Добавлена фукция смены местами данных в шаблонах
    Работа:
    #43

--- fp/fplk/app/view/orders/TemplWin.js
+++ fp/fplk/app/view/orders/TemplWin.js
@@ -1,29 +1,33 @@
 Ext.define('fplk.view.orders.TemplWin', {
-	extend : 'Ext.Window',
-	extend : 'Ext.window.Window',
-	alias : 'widget.templwin',
-	requires : ['fplk.view.orders.TemplForm'],
-	title : 'Новый шаблон',
-	layout : 'vbox',
-	autoShow : true,
-	height : 500,
-	width : 770,
-	resizable : false,
-	modal : true,
-	initComponent : function () {
-		this.items = [{
-				xtype : 'templform'				
-			}
-		];
-		this.buttons = [{
-				text : 'Сохранить',
-				action : 'save'
-			}, {
-				text : 'Отмена',
-				scope : this,
-				handler : this.close
-			}
-		];
-		this.callParent(arguments);
-	}
+    extend: 'Ext.Window',
+    extend: 'Ext.window.Window',
+    alias: 'widget.templwin',
+    requires: ['fplk.view.orders.TemplForm'],
+    title: 'Новый шаблон',
+    layout: 'vbox',
+    autoShow: true,
+    height: 500,
+    width: 770,
+    resizable: false,
+    modal: true,
+    initComponent: function () {
+        this.items = [{
+            xtype: 'templform'
+        }
+        ];
+        this.buttons = [{
+            text: 'Поменять',
+            action: 'swap',
+            hidden: true
+        }, {
+            text: 'Сохранить',
+            action: 'save'
+        }, {
+            text: 'Отмена',
+            scope: this,
+            handler: this.close
+        }
+        ];
+        this.callParent(arguments);
+    }
 });
