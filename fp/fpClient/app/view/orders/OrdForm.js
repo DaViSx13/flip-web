8caa3598ab88bada56498081f0eb04ce34d20487
tgedanke <tgedanke@gmail.com>
2022-12-13 09:06:25 +0300

    fix

--- fp/fpClient/app/view/orders/OrdForm.js
+++ fp/fpClient/app/view/orders/OrdForm.js
@@ -35,7 +35,7 @@ Ext.define('FPClient.view.orders.OrdForm', {
 					xtype : 'textfield',
 					width : 337,
 					name : 'address',
-					maxLength : 70,
+					maxLength : 200,
 					fieldLabel : 'Адрес',
 					labelAlign : 'top',
 					_allowBlank : false
@@ -96,7 +96,7 @@ Ext.define('FPClient.view.orders.OrdForm', {
 					xtype : 'textfield',
 					width : 337,
 					name : 'dadr',
-					maxLength : 70,
+					maxLength : 200,
 					fieldLabel : 'Адрес',
 					labelAlign : 'top',
 					allowBlank : false
