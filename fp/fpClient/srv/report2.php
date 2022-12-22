1d84c164c69bb2c55c8722c39a29ce06303716c5
vb010894 <aleksiev_vova@mail.ru>
2022-12-22 17:38:59 +0300

    Изменено  Кабинет Агента   - Включен продуктивный сервер печати отчетов  Проверка:   - Формирования отчета   - Требуется проверка отчета на сервере для совместимости #47

--- fp/fpClient/srv/report2.php
+++ fp/fpClient/srv/report2.php
@@ -7,22 +7,22 @@
 // Боевой сервер
 // TODO Расскоментировать перед выставлением
 
-    /*$url = 'http://jasperadmin:jasperadmin@10.10.10.6:8080'.
-        '/jasperserver'.
-        '/rest_v2'.
-        '/reports'.
-        '/flippost'.
-        '/reports'.
-        '/wbreportGROUP.pdf?wbNo='.$wbno;*/
-
-    // Локальный сервер
-    $url = 'http://jasperadmin:jasperadmin@localhost:8080' .
+    $url = 'http://jasperadmin:jasperadmin@10.10.10.6:8080'.
         '/jasperserver'.
         '/rest_v2'.
         '/reports'.
         '/flippost'.
         '/reports'.
         '/wbreportGROUP.pdf?wbNo='.$wbno;
+
+    // Локальный сервер
+    /*$url = 'http://jasperadmin:jasperadmin@localhost:8080' .
+        '/jasperserver'.
+        '/rest_v2'.
+        '/reports'.
+        '/flippost'.
+        '/reports'.
+        '/wbreportGROUP.pdf?wbNo='.$wbno;*/
     $ch = curl_init($url);
     $fh = fopen('php://temp', 'w');
     curl_setopt($ch, CURLOPT_FILE, $fh);
