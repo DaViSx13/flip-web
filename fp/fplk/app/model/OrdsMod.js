5d4eb314f04945216700ca841e9568f6a6441233
vb010894 <aleksiev_vova@mail.ru>
2023-10-06 03:06:55 +0300

    FLIP-102 ЛК Москвы. Добавить колонку "Примечание" Изменено: 1) Добавлена колонка "Примечание" в таблице "Заказы" 2) Добавлен tooltip с полным текстом описания

--- fp/fplk/app/model/OrdsMod.js
+++ fp/fplk/app/model/OrdsMod.js
@@ -1,80 +1,83 @@
 Ext.define('fplk.model.OrdsMod', {
-	extend : 'Ext.data.Model',
-	fields : [{
-			name : 'rordnum'
-		}, {
-			name : 'status',
-			type : 'string'
-		}, {
-			name : 'datein',
-			type : 'date',
-			dateFormat : 'Y-m-d H:i:s'
-		}, {
-			name : 'courdate',
-			type : 'date',
-			dateFormat : 'Y-m-d H:i:s'
-		}, {
-			name : 'orgcity'
-		}, {
-			name : 'cname'
-		}, {
-			name : 's_adr',
-			type : 'string'
-		}, {
-			name : 's_name',
-			type : 'string'
-		}, {
-			name : 's_mail',
-			type : 'string'
-		}, {
-			name : 's_tel',
-			type : 'string'
-		}, {
-			name : 'destcity'
-		}, {
-			name : 'dname'
-		}, {
-			name : 'type'
-		}, {
-			name : 'packs',
-			type : 'int'
-		}, {
-			name : 'wt',
-			type : 'float'
-		}, {
-			name : 'volwt',
-			type : 'float'
-		}, {
-			name : 'wb_no',
-			type : 'string'
-		}, {
-			name : 'org',
-			type : 'int'
-		}, {
-			name:'isex',
-			type: 'int'
-		}, {
-			name : 'fpayr',
-			type : 'int'
-		}, {
-			name : 'metpaym'
-		}, {
-			name : 'sberquick',
-			type : 'int'
-		}, {
-			name : 'sbersize',
-			type : 'string'
-	}, {
-		name : 'sbercost',
-		type : 'float'
-	}, {
-		name : 'sberproject',
-		type : 'string'
-	}, {
-		name : 'sbersuit',
-		type : 'string'
-	}, {
-		name : 'sbertype',
-		type : 'string'
-	}]
+    extend: 'Ext.data.Model',
+    fields: [{
+        name: 'rordnum'
+    }, {
+        name: 'status',
+        type: 'string'
+    }, {
+        name: 'datein',
+        type: 'date',
+        dateFormat: 'Y-m-d H:i:s'
+    }, {
+        name: 'courdate',
+        type: 'date',
+        dateFormat: 'Y-m-d H:i:s'
+    }, {
+        name: 'orgcity'
+    }, {
+        name: 'cname'
+    }, {
+        name: 's_adr',
+        type: 'string'
+    }, {
+        name: 's_name',
+        type: 'string'
+    }, {
+        name: 'orgrems',
+        type: 'string'
+    }, {
+        name: 's_mail',
+        type: 'string'
+    }, {
+        name: 's_tel',
+        type: 'string'
+    }, {
+        name: 'destcity'
+    }, {
+        name: 'dname'
+    }, {
+        name: 'type'
+    }, {
+        name: 'packs',
+        type: 'int'
+    }, {
+        name: 'wt',
+        type: 'float'
+    }, {
+        name: 'volwt',
+        type: 'float'
+    }, {
+        name: 'wb_no',
+        type: 'string'
+    }, {
+        name: 'org',
+        type: 'int'
+    }, {
+        name: 'isex',
+        type: 'int'
+    }, {
+        name: 'fpayr',
+        type: 'int'
+    }, {
+        name: 'metpaym'
+    }, {
+        name: 'sberquick',
+        type: 'int'
+    }, {
+        name: 'sbersize',
+        type: 'string'
+    }, {
+        name: 'sbercost',
+        type: 'float'
+    }, {
+        name: 'sberproject',
+        type: 'string'
+    }, {
+        name: 'sbersuit',
+        type: 'string'
+    }, {
+        name: 'sbertype',
+        type: 'string'
+    }]
 });
--- fp/fplk/app/view/orders/OrdGrid.js
+++ fp/fplk/app/view/orders/OrdGrid.js
@@ -1,102 +1,111 @@
 Ext.define('fplk.view.orders.OrdGrid', {
-	extend : 'Ext.grid.Panel',
-	alias : 'widget.ordgrid',
-	requires : ['fplk.view.orders.OrdTool', 'fplk.view.orders.OrdTotal'],
-	store : 'OrdsSt',
-	viewConfig: {
-		enableTextSelection: true
-	},
-	columns : [{
-		xtype: 'actioncolumn',
-		text: 'ИС',
-		name: 'exaction',
-		width: 40,
-		dataIndex: 'is_ex',
-		items: [{
-				getClass: function (v, meta, rec) {
-					if (rec.get('isex') > 0) {
-						this.items[0].tooltip = 'Посмотреть ИС';
-						return 'ex-col';
-					}
-				},
-				handler: function (grid, rowIndex, colIndex, node, e, record) {
-					var action = 'exaction';
-					this.fireEvent('itemclick', this, action, grid, rowIndex, colIndex, record, node);
-				}
-		}]
-		},{
-			xtype : 'gridcolumn',
-			width : 60,
-			dataIndex : 'rordnum',
-			text : '№'
-		}, {
-			xtype : 'gridcolumn',
-			width : 150,
-			dataIndex : 'status',
-			text : 'Статус'
-		}, {
-			xtype : 'datecolumn',
-			format : 'd.m.Y',
-			width : 70,
-			dataIndex : 'datein',
-			text : 'Дата ввода'
-		}, {
-			xtype : 'datecolumn',
-			format : 'd.m.Y',
-			width : 80,
-			dataIndex : 'courdate',
-			text : 'Дата приезда'
-		}, {
-			xtype : 'gridcolumn',
-			width : 114,
-			dataIndex : 'orgcity',
-			text : 'Город'
-		}, {
-			xtype : 'gridcolumn',
-			flex : 1,
-			dataIndex : 'cname',
-			text : 'Отправитель'
-		}, {
-			xtype : 'gridcolumn',
-			width : 114,
-			dataIndex : 'destcity',
-			text : 'Город'
-		}, {
-			xtype : 'gridcolumn',
-			flex : 1,
-			dataIndex : 'dname',
-			text : 'Получатель'
-		}, {
-			xtype : 'numbercolumn',
-			format : '0',
-			width : 41,
-			dataIndex : 'packs',
-			text : 'Кол.'
-		}, {
-			xtype : 'numbercolumn',
-			format : '0.00',
-			width : 51,
-			dataIndex : 'wt',
-			text : 'Вес'
-		}, {
-			xtype : 'numbercolumn',
-			format : '0.00',
-			width : 51,
-			dataIndex : 'volwt',
-			text : 'Об. вес'
-		}, {
-			xtype : 'gridcolumn',
-			width : 70,
-			dataIndex : 'wb_no',
-			text : '№ накл.'
-		}
-	],
-	dockedItems : [{
-			xtype : 'ordtool',
-			dock : 'top'
-		}, {
-			xtype : 'ordtotal',
-			dock : 'bottom'
-		}
-	]
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.ordgrid',
+    requires: ['fplk.view.orders.OrdTool', 'fplk.view.orders.OrdTotal'],
+    store: 'OrdsSt',
+    viewConfig: {
+        enableTextSelection: true
+    },
+    columns: [{
+        xtype: 'actioncolumn',
+        text: 'ИС',
+        name: 'exaction',
+        width: 40,
+        dataIndex: 'is_ex',
+        items: [{
+            getClass: function (v, meta, rec) {
+                if (rec.get('isex') > 0) {
+                    this.items[0].tooltip = 'Посмотреть ИС';
+                    return 'ex-col';
+                }
+            },
+            handler: function (grid, rowIndex, colIndex, node, e, record) {
+                var action = 'exaction';
+                this.fireEvent('itemclick', this, action, grid, rowIndex, colIndex, record, node);
+            }
+        }]
+    }, {
+        xtype: 'gridcolumn',
+        width: 60,
+        dataIndex: 'rordnum',
+        text: '№'
+    }, {
+        xtype: 'gridcolumn',
+        width: 150,
+        dataIndex: 'status',
+        text: 'Статус'
+    }, {
+        xtype: 'datecolumn',
+        format: 'd.m.Y',
+        width: 70,
+        dataIndex: 'datein',
+        text: 'Дата ввода'
+    }, {
+        xtype: 'datecolumn',
+        format: 'd.m.Y',
+        width: 80,
+        dataIndex: 'courdate',
+        text: 'Дата приезда'
+    }, {
+        xtype: 'gridcolumn',
+        width: 114,
+        dataIndex: 'orgcity',
+        text: 'Город'
+    }, {
+        xtype: 'gridcolumn',
+        flex: 1,
+        dataIndex: 'cname',
+        text: 'Отправитель'
+    }, {
+        xtype: 'gridcolumn',
+        width: 250,
+        dataIndex: 'orgrems',
+        text: 'Примечание',
+        renderer: function (value, meta) {
+            meta.tdAttr = 'data-qtip="' + value + '"';
+            return value;
+        }
+    }, {
+        xtype: 'gridcolumn',
+        width: 114,
+        dataIndex: 'destcity',
+        text: 'Город'
+    }, {
+        xtype: 'gridcolumn',
+        flex: 1,
+        dataIndex: 'dname',
+        text: 'Получатель'
+    }, {
+        xtype: 'numbercolumn',
+        format: '0',
+        width: 41,
+        dataIndex: 'packs',
+        text: 'Кол.'
+    }, {
+        xtype: 'numbercolumn',
+        format: '0.00',
+        width: 51,
+        dataIndex: 'wt',
+        text: 'Вес'
+    }, {
+        xtype: 'numbercolumn',
+        format: '0.00',
+        width: 51,
+        dataIndex: 'volwt',
+        text: 'Об. вес'
+    }, {
+        xtype: 'gridcolumn',
+        width: 70,
+        dataIndex: 'wb_no',
+        text: '№ накл.'
+    }
+    ],
+    dockedItems: [{
+        xtype: 'ordtool',
+        dock: 'top'
+    }, {
+        xtype: 'ordtotal',
+        dock: 'bottom'
+    }
+    ]
 });
