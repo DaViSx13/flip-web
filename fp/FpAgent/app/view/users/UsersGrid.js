ac4b14125c564ee162dd4f6ecc35173813877a3d
vb010894 <aleksiev_vova@mail.ru>
2023-11-21 00:47:17 +0300

    FLIP-106 Все Лк сделать нумерацию пользователей
    Изменено
    1) Добавлена нумерация в кабинете Агента и Клиента

--- fp/FpAgent/app/view/users/UsersGrid.js
+++ fp/FpAgent/app/view/users/UsersGrid.js
@@ -1,54 +1,58 @@
 Ext.define('FPAgent.view.users.UsersGrid', {
-	extend : 'Ext.grid.Panel',
-	alias : 'widget.usersgrid',
-	requires : ['FPAgent.view.users.UsersTool'],
-	store : 'UsersSt',
-	columns : [{
-			text : FPAgent.lib.Translate.tr("UsersGrid.auser"),//'Логин',
-			dataIndex : 'auser',
-			flex : 1
-		}, {
-			text : FPAgent.lib.Translate.tr("UsersGrid.partname"),//'Пользователь',
-			flex : 1,
-			dataIndex : 'partname'
-		}, {
-			xtype : 'actioncolumn',
-			text : FPAgent.lib.Translate.tr("UsersGrid.isblocked"),//'Доступ',
-			align : 'center',
-			width : 50,
-			getClass : function (v, meta, rec) {
-				if (rec.get('active') > 0) {
-					this.items[0].tooltip = FPAgent.lib.Translate.tr("UsersGrid.block_no");//'Не блокирован';
-					return 'gre-usr';
-				} else {
-					this.items[0].tooltip = FPAgent.lib.Translate.tr("UsersGrid.block_yes");//'Блокирован';
-					return 'red-usr';
-				}
-			}
-		}, {
-			text : FPAgent.lib.Translate.tr("UsersGrid.partloc"),//'КОД',
-			width : 50,
-			dataIndex : 'partloc'
-		}, {
-			text : FPAgent.lib.Translate.tr("ComboCity"),//'Город',
-			width : 150,
-			dataIndex : 'rusname'
-		}, {
-			xtype : 'actioncolumn',
-			text : FPAgent.lib.Translate.tr("UsersGrid.status"),//'Статус',
-			align : 'center',
-			width : 50,
-			getClass : function (v, meta, rec) {
-				if (rec.get('dateshtdn') > '') {
-					this.items[0].tooltip = FPAgent.lib.Translate.tr("UsersGrid.status.tooltip")/*'Отношения прекращены: '*/ + rec.get('dateshtdn');
-					return 'cut';
-				}
-			}
-		}
-	],
-	dockedItems : [{
-			xtype : 'userstool',
-			dock : 'top'
-		}
-	]
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.usersgrid',
+    requires: ['FPAgent.view.users.UsersTool'],
+    store: 'UsersSt',
+    columns: [{
+        xtype: 'rownumberer',
+        resizable: true,
+        width: 30
+    }, {
+        text: FPAgent.lib.Translate.tr("UsersGrid.auser"),//'Логин',
+        dataIndex: 'auser',
+        flex: 1
+    }, {
+        text: FPAgent.lib.Translate.tr("UsersGrid.partname"),//'Пользователь',
+        flex: 1,
+        dataIndex: 'partname'
+    }, {
+        xtype: 'actioncolumn',
+        text: FPAgent.lib.Translate.tr("UsersGrid.isblocked"),//'Доступ',
+        align: 'center',
+        width: 50,
+        getClass: function (v, meta, rec) {
+            if (rec.get('active') > 0) {
+                this.items[0].tooltip = FPAgent.lib.Translate.tr("UsersGrid.block_no");//'Не блокирован';
+                return 'gre-usr';
+            } else {
+                this.items[0].tooltip = FPAgent.lib.Translate.tr("UsersGrid.block_yes");//'Блокирован';
+                return 'red-usr';
+            }
+        }
+    }, {
+        text: FPAgent.lib.Translate.tr("UsersGrid.partloc"),//'КОД',
+        width: 50,
+        dataIndex: 'partloc'
+    }, {
+        text: FPAgent.lib.Translate.tr("ComboCity"),//'Город',
+        width: 150,
+        dataIndex: 'rusname'
+    }, {
+        xtype: 'actioncolumn',
+        text: FPAgent.lib.Translate.tr("UsersGrid.status"),//'Статус',
+        align: 'center',
+        width: 50,
+        getClass: function (v, meta, rec) {
+            if (rec.get('dateshtdn') > '') {
+                this.items[0].tooltip = FPAgent.lib.Translate.tr("UsersGrid.status.tooltip")/*'Отношения прекращены: '*/ + rec.get('dateshtdn');
+                return 'cut';
+            }
+        }
+    }
+    ],
+    dockedItems: [{
+        xtype: 'userstool',
+        dock: 'top'
+    }
+    ]
 });
