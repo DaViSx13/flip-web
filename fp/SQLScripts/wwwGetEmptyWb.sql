1b218bdf214bcfbdf1ce6c131c4c6e7bcacb691c
vb010894 <aleksiev_vova@mail.ru>
2023-12-26 10:24:57 +0300

    FLIP-109 ЛК АГЕНТА. Печать накладной
    Изменено
    1) Создана процедура получания накладной из заказа

--- fp/SQLScripts/wwwGetEmptyWb.sql
+++ fp/SQLScripts/wwwGetEmptyWb.sql
@@ -7,16 +7,16 @@ AS
             SELECT
                 Wb_no = 'AZ' + LTRIM(STR(@Order)),      -- 1. Номер накладной
 
-                        agOrders.DateIn,                        -- 2. Дата внесения в систему
-                        agOrders.ORG,                           -- 3. Город отправителя
-                        agOrders.DEST,                          -- 4. Город получателя
-                        agOrders.ContName   AS S_Name,          -- 5. Имя контакта отправителя
-                        agOrders.ContPhone  AS S_Tel,           -- 6. Телефон отправителя
-                        agOrders.CName      AS S_Co,            -- 7. Компания отправитель
-                        agOrders.Address    AS S_Adr,           -- 8. Адрес отправителя
+                agOrders.DateIn,                        -- 2. Дата внесения в систему
+                agOrders.ORG,                           -- 3. Город отправителя
+                agOrders.DEST,                          -- 4. Город получателя
+                agOrders.ContName   AS S_Name,          -- 5. Имя контакта отправителя
+                agOrders.ContPhone  AS S_Tel,           -- 6. Телефон отправителя
+                agOrders.CName      AS S_Co,            -- 7. Компания отправитель
+                agOrders.Address    AS S_Adr,           -- 8. Адрес отправителя
                 S_Cnt = scity.country,                  -- 9. Страна отправителя
                 S_Zip = '''',                           -- 10. Индекс отправителя
-                        agOrders.OrgRems    AS S_Ref,           -- 11. Примечания отправителя
+                agOrders.OrgRems    AS S_Ref,           -- 11. Примечания отправителя
                 S_City = scity.city,                    -- 12. Город отправителя
 
                 agOrders.DContName  AS R_Name,          -- 13. Имя контакта получателя
@@ -34,7 +34,7 @@ AS
                     WHEN 1 THEN 'LE'
                     WHEN 0 THEN 'PL'
                     ELSE 'PA'
-                            END                 AS T_PAK,           -- 24. Упаковка
+                END                 AS T_PAK,           -- 24. Упаковка
                 '' AS T_DEL,                            -- 24. Тип доставки
                 '' AS Pers,                             -- 25. Лично в руки
                 agOrders.Packs      AS PCS,             -- 26. Количество мест
@@ -59,16 +59,16 @@ AS
             SELECT
                 Wb_no = 'KZ' + LTRIM(STR(@Order)),       -- 1. Номер накладной
 
-                        regOrders.DateIn,                        -- 2. Дата внесения в систему
-                        regOrders.ORG,                           -- 3. Город отправителя
-                        regOrders.DEST,                          -- 4. Город получателя
-                        regOrders.ContName   AS S_Name,          -- 5. Имя контакта отправителя
-                        regOrders.ContPhone  AS S_Tel,           -- 6. Телефон отправителя
-                        regOrders.CName      AS S_Co,            -- 7. Компания отправитель
-                        regOrders.Address    AS S_Adr,           -- 8. Адрес отправителя
+                regOrders.DateIn,                        -- 2. Дата внесения в систему
+                regOrders.ORG,                           -- 3. Город отправителя
+                regOrders.DEST,                          -- 4. Город получателя
+                regOrders.ContName   AS S_Name,          -- 5. Имя контакта отправителя
+                regOrders.ContPhone  AS S_Tel,           -- 6. Телефон отправителя
+                regOrders.CName      AS S_Co,            -- 7. Компания отправитель
+                regOrders.Address    AS S_Adr,           -- 8. Адрес отправителя
                 S_Cnt = scity.country,                   -- 9. Страна отправителя
                 S_Zip = '''',                            -- 10. Индекс отправителя
-                        regOrders.OrgRems    AS S_Ref,           -- 11. Примечания отправителя
+                regOrders.OrgRems    AS S_Ref,           -- 11. Примечания отправителя
                 S_City = scity.city,                     -- 12. Город отправителя
 
                 regOrders.DContName  AS R_Name,          -- 13. Имя контакта получателя
@@ -86,7 +86,7 @@ AS
                     WHEN 1 THEN 'LE'
                     WHEN 0 THEN 'PL'
                     ELSE 'PA'
-                            END                 AS T_PAK,            -- 24. Упаковка
+                END                 AS T_PAK,            -- 24. Упаковка
                 '' AS T_DEL,                             -- 24. Тип доставки
                 '' AS Pers,                              -- 25. Лично в руки
                 regOrders.Packs      AS PCS,             -- 26. Количество мест
@@ -103,9 +103,8 @@ AS
                 regOrders.DESTRems,                      -- 37. Примечания к получению,
                 '2'                 AS acc               -- 38. АСС
             FROM RegOrders regOrders
-                     LEFT JOIN vCity scity ON regOrders.ORGCity = scity.cityId
-                     LEFT JOIN vCity rcity ON regOrders.DEST = rcity.cityId
-
+            LEFT JOIN vCity scity ON regOrders.ORGCity = scity.cityId
+            LEFT JOIN vCity rcity ON regOrders.DEST = rcity.cityId
         END
 
 GO
