e899df9e1a0092f9ecb90edf96d3afb5af7898ed
vb010894 <aleksiev_vova@mail.ru>
2024-02-21 14:53:13 +0300

    FLIP-112 ЛК Москвы импорт пользователей
    1) Добавлен механизм импорта пользователей

--- fp/fplk/srv/import/import.php
+++ fp/fplk/srv/import/import.php
@@ -4,8 +4,8 @@
 *
 */
 require_once('importerToDB.php');
-//session_name("LKSESSIONID");
-//session_start();
+session_name("LKSESSIONID");
+session_start();
 error_reporting(0);
 function isSessionActive(){
 	return isset($_SESSION['xUser']);
@@ -14,9 +14,10 @@ function isSessionActive(){
 if (!ini_get('file_uploads')){
 	ini_set('file_uploads', '1');
 }
-//if (!isSessionActive()){
-				//throw new Exception('Сеанс завершен. Обновите страницу.');
-				//};
+
+if (!isSessionActive()){
+				throw new Exception('Сеанс завершен. Обновите страницу.');
+				};
 $importer = new importerToDB();
 $importer->file = $_FILES['uploadFile'];
 $importer->action = $_POST['act'];
